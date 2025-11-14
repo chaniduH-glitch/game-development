f0255474cd7db1cc381e01b9a40e635dacd65f91
chaniduH-glitch <chaniduhuruggamuwa@gmail.com>
2025-11-14 17:57:48 +0000

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dict1 = { "a" : 1 , "b": 2, "c" : 3 }
+print(dict1)
+list1 = ["a","tree","apple","book","orange"]
+print(list1[2])
+#get the value with the help of key {"key":value}
+print(dict1["b"])
+print(dict1["c"])
+
