dda32fa2c61116d5a05892f0dcfe46ca11b4ac16
chaniduH-glitch <chaniduhuruggamuwa@gmail.com>
2026-01-02 17:25:08 +0000

    Add files via upload

---
+++ sports.py
@@ -0,0 +1,17 @@
+badminton = {11,10,12,13,15}
+soccer = {8,9,10,12,13}
+
+print(badminton.intersection(soccer))
+print(badminton & soccer)
+
+print(badminton.symmetric_difference(soccer))
+print(badminton^soccer)
+
+print(badminton.difference(soccer))
+print(badminton-soccer)
+
+
+
+
+
+ 
